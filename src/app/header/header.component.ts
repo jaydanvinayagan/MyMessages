d4442e31ef5878429c2408b83aaa78313c2cd264
DIDDLY\Jaydan <jvinayagan@gmail.com>
2019-03-05 22:23:52 +1300

    Implementing Database connections
    Added deleting of messages from mesage board

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,6 +5,6 @@ import {Component} from '@angular/core';
   templateUrl: './header.component.html'
 })
 
-export class HeaderComponent{
+export class HeaderComponent {
 
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -30,6 +30,9 @@ export class PostListComponent implements OnInit, OnDestroy {
             this.posts = posts;
           });
   }
+  onDelete(postId: string){
+    this.postsService.deletePost(postId);
+  }
 
   ngOnDestroy() {
     this.postsSub.unsubscribe();
